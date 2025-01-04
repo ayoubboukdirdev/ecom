f17cb9330359b1766f0c71aa508500580bdc48c3
AYOUB BOUKDIR <business.ayoubboukdir@gmail.com>
2025-01-04 16:25:37 +0100

    create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'ECOMAROC'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'Welcome to Ecomaroc. The best place to buy items'
